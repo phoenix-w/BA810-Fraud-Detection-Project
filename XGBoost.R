b1a8847858e489d586fca83af9fe08f6ade6180f
phoenix-w <phoenix-w@users.noreply.github.com>
2021-02-10 21:31:23 -0500

    Update XGBoost.R

--- XGBoost.R
+++ XGBoost.R
@@ -1,3 +1,7 @@
+# https://www.analyticsvidhya.com/blog/2016/01/xgboost-algorithm-easy-steps/
+# https://www.kaggle.com/gpreda/credit-card-fraud-detection-using-xgboost
+# https://www.kaggle.com/bonovandoo/fraud-detection-with-smote-and-xgboost-in-r
+
 library(data.table)
 library(xgboost)
 library(readr)
