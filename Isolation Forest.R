991f73ae71d5481e8142ba753a66607108bae97f
jeleung2 <jeffreykkleung@gmail.com>
2021-02-25 17:21:00 -0800

    Lasso update

--- Isolation Forest.R
+++ Isolation Forest.R
@@ -25,29 +25,25 @@ iso <- isolationForest$new(sample_size = length(iforest_train))
 # fit for data
 iso$fit(iforest_train)
 
-
 # Obtain anomaly scores (According to documentation: "If the score is closer to 1 for a some observations, they are likely outliers. If the score for all observations hover around 0.5, there might not be outliers at all.")
 iforest_scores_train = iso$predict(iforest_train)
 iforest_scores_train[order(anomaly_score, decreasing = TRUE)]
-iforest_train$predictions <- as.factor(ifelse(iforest_scores_train$anomaly_score >=0.60, 1, 0))
+iforest_train$predictions <- as.factor(ifelse(iforest_scores_train$anomaly_score >=0.6, 1, 0))
 
 iforest_scores_test = iso$predict(iforest_test)
 iforest_scores_test[order(anomaly_score, decreasing = TRUE)]
-iforest_test$predictions <- as.factor(ifelse(iforest_scores_test$anomaly_score >=0.60, 1, 0))
+iforest_test$predictions <- as.factor(ifelse(iforest_scores_test$anomaly_score >=0.6, 1, 0))
 
 
-# Train dataset True Positive and True Negative
-train.fraud.accuracy <- sum(iforest_train$Class[iforest_train$outliers == "outlier"])/sum(iforest_train$Class)
-train.real.accuracy <- length(iforest_train$Class[iforest_train$Class == 0 & iforest_train$outlier == "normal"])/sum(iforest_train$Class == 0)
-
+# Confusion Matrix and ROC of training data
 confusionMatrix(iforest_train$predictions, as.factor(train$Class), positive = "1")
+roc.curve(train$Class, iforest_train$predictions, plotit = TRUE)
 
+# Confusion Matrix and ROC of test data
 confusionMatrix(iforest_test$predictions, as.factor(test$Class), positive = "1")
-
-# Test dataset True Positive and True Negative
-test.fraud.accuracy <- sum(iforest_test$Class[iforest_test$outlier == "outlier"])/sum(iforest_test$Class)
-test.real.accuracy <- length(iforest_test$Class[iforest_test$Class == 0 & iforest_test$outlier == "normal"])/sum(iforest_test$Class == 0)
-
 roc.curve(test$Class, iforest_test$predictions, plotit = TRUE)
 
-roc.curve(train$Class, iforest_train$predictions, plotit = TRUE)
+
+
+
+
---
+++ Lasso Binomial.R
@@ -0,0 +1,76 @@
+library(data.table)
+library(caTools)
+library(xgboost)
+library(caret)
+library(ROCR)
+library("pROC")
+library("ROSE")
+library("magrittr")
+library(glmnet)
+credit_card_raw <- fread("/Users/jeffrey/Documents/Boston University/BU-QST-Masters/Spring 2020/BA810/Team Project/Data/creditcard.csv")
+
+
+# Create train and test dataset
+credit_card_raw[, test:=0]
+credit_card_raw[, "Time":= NULL]
+credit_card_raw[sample(nrow(credit_card_raw), 284807*0.2), test:=1]
+test <- credit_card_raw[test==1]
+train <- credit_card_raw[test==0]
+train[, "test" := NULL]
+test[, "test" := NULL]
+credit_card_raw[, "test" := NULL]
+
+# Convert datatables to dataframes for downsampling
+setDF(train)
+setDF(test)
+
+# Downsample
+set.seed(1)
+train$Class <- factor(train$Class)
+downsample.train <- downSample(train[, -ncol(train)], train$Class)
+
+test$Class <- factor(test$Class)
+downsample.test <- downSample(test[, -ncol(test)], test$Class)
+
+
+# Create formula
+formula <- as.formula(Class ~ .)
+
+# Downsample training set modeling 
+downsample.train.matrix <- model.matrix(formula, downsample.train)[, -1]
+y.downsample.train <- downsample.train$Class
+downsample.fit <- cv.glmnet(downsample.train.matrix, y.downsample.train, family = "binomial", alpha = 1, nfolds = 10)
+
+# Create testing matrices
+downsample.test.matrix <- model.matrix(formula, downsample.test) [, -1]
+imbalanced.test.matrix <- model.matrix(formula, test)[, -1]
+
+# Predicting Downsample test data
+downsample.test.predictions <- predict(downsample.fit, downsample.test.matrix, s = downsample.fit$lambda.min) 
+predicted.classes <- ifelse(downsample.test.predictions > 0, 1, 0)
+confusionMatrix(as.factor(predicted.classes), downsample.test$Class, positive = "1")
+roc.curve(as.numeric(downsample.test$Class), as.numeric(predicted.classes), plotit = TRUE)
+
+# Predicting imbalanced test data
+test.predictions <- predict(downsample.fit, imbalanced.test.matrix, s = downsample.fit$lambda.min) 
+predicted.classes <- ifelse(test.predictions > 0, 1, 0)
+confusionMatrix(as.factor(predicted.classes), test$Class, positive = "1")
+roc.curve(as.numeric(test$Class), as.numeric(predicted.classes), plotit = TRUE)
+
+# Imbalanced training set modeling
+class_0 = copy(train[train$Class == 0,])
+x <- copy(class_0[sample(nrow(class_0), 4000),])
+imbalanced.train <- rbind(x, train[train$Class == 1,])
+train.matrix <- model.matrix(formula, imbalanced.train)[, -1]
+y.train <- imbalanced.train$Class
+imbalanced.fit <- cv.glmnet(train.matrix, y.train, family = "binomial", alpha = 1, nfolds = 10)
+
+# Predicting Downsample test data
+downsample.test.predictions <- predict(imbalanced.fit, downsample.test.matrix, s = imbalanced.fit$lambda.min) 
+predicted.classes <- ifelse(downsample.test.predictions > 0, 1, 0)
+confusionMatrix(as.factor(predicted.classes), downsample.test$Class, positive = "1")
+
+#Predicting imbalanced test data
+test.predictions <- predict(imbalanced.fit, imbalanced.test.matrix, s = imbalanced.fit$lambda.min) 
+predicted.classes <- ifelse(test.predictions > 0, 1, 0)
+confusionMatrix(as.factor(predicted.classes), test$Class, positive = "1")
